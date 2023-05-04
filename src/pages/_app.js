b23422f8446789fa1cfc8409ea41af788c23593a
galenomoon <gui.galenocover@gmail.com>
2023-05-03 21:04:55 -0300

    register page + termos de uso rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import Image from 'next/image'
+import background_base from '@/assets/base-light.svg'
+import background_base_dark from '@/assets/base-dark.svg'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const isDarkTheme = false
+
+  return (
+    <div className={isDarkTheme ? "dark" : ""}>
+      <div className='text-typography-base dark:text-dark-typography-base relative overflow-hidden flex flex-col w-full h-fit overflow-y-auto'>
+        <Image src={background_base} alt="bg-base" fill className="dark:hidden h-full absolute z-0 object-cover" />
+        <Image src={background_base_dark} alt="bg-base-dark" fill className="dark:block hidden absolute z-0 object-fill " />
+        <div className='z-[10]'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,45 @@
+
+import React from "react";
+
+//assets
+import mac from "../assets/mac.png"
+import Link from "next/link";
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex w-full h-fit flex-col overflow-hidden">
       <Header />
-      oi
+      <div className="flex md:flex-row sm:flex-col-reverse w-full sm:h-[65vh] md:h-[80vh]">
+        <div className="flex w-full items-center justify-center  sm:p-0 md:pl-8">
+          <div className="flex flex-col gap-2 animate-slide-in-r md:text-start sm:text-center">
+            <span className="font-bold sm:text-3xl md:text-6xl">
+              Encontre os <span className="text-typography-primary dark:text-dark-typography-primary">melhores</span><br />
+              prestadores de serviços<br /> para a <span className="text-typography-primary dark:text-dark-typography-primary">sua obra!</span>
+            </span>
+            <p className="text-typography-light dark:text-dark-typography-light sm:text-sm md:text-xl">
+              Trabalhamos com prestadores de serviços qualificados e comprometidos. <br className="md:block sm:hidden" />
+              Descubra como podemos ajudá-lo a tornar seus projetos de construção realidade
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-center sm:flex md:relative md:h-auto sm:h-full">
+          <div className="absolute z-[9] sm:self-center md:left-auto sm:w-[250px] md:w-[500px] sm:h-[250px] md:h-[500px] bg-background-secondary rounded-full animate-slide-in-b" />
+          <Image src={mac} alt="mac" className="z-[10] animate-slide-in-l sm:max-h-[400px] md:max-h-[500px]" />
+        </div>
+      </div>
+      <div className="text-center flex flex-col h-fit gap-2 py-[50px] pb-[100px] animate-slide-in-t">
+        <span className="font-bold sm:text-3xl md:text-6xl">
+          Mais de <Link href="/categorias" className="text-typography-primary dark:text-dark-typography-primary hover:underline">
+            1325
+          </Link> empresas registradas
+        </span>
+        <p className="text-typography-light dark:text-dark-typography-light sm:text-sm md:text-xl">
+          Clique no link acima ou <Link href="/categorias" className="text-typography-primary dark:text-dark-typography-primary underline">clique aqui</Link> para ver todas as categorias disponíveis
+        </p>
+      </div>
     </main>
-  )
+  );
 }
