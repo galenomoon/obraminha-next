4cd729e91a6e6088e44b95a35ceb23f7cec106bd
galenomoon <gui.galenocover@gmail.com>
2023-05-16 19:48:19 -0300

    trying again

--- src/pages/categorias/[category_slug]/[address_slug]/index.jsx
+++ src/pages/categorias/[category_slug]/[address_slug]/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import OrganizationsPage from '../index'
 
-export default function OrganizationsByAddressSlug() {
-  return <OrganizationsPage search_by_address />
+export default function OrganizationsByAddressSlug({params}) {
+  return <OrganizationsPage search_by_address={params?.address_slug} />
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      params
+    },
+    revalidate: 1
+  }
 }
 
 export async function getStaticPaths() {
-  return { fallback: 'true' };
+  return { paths: [], fallback: true };
 }
