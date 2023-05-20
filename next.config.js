7ca9c0f15cf0dfedb5c09d449d5f0032454e0862
galenomoon <gui.galenocover@gmail.com>
2023-05-19 22:29:31 -0300

    removing export of next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   reactStrictMode: true,
   env: {
     REACT_APP_API_URL: process.env.REACT_APP_API_URL,
