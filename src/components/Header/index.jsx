cad299f54ee9e47468293773569ee530364991d7
galenomoon <gui.galenocover@gmail.com>
2023-05-19 23:41:59 -0300

    fix description

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 import Head from 'next/head'
 
-export default function Header({ title = "Encontre os melhores prestadores de serviços para a sua obra!", subtitle, description = "Trabalhamos com prestadores de serviços qualificados e comprometidos. Descubra como podemos ajudá-lo a tornar seus projetos de construção realidade" }) {
+export default function Header({ title = "Encontre os melhores prestadores de serviços para a sua obra!", subtitle, description }) {
   return (
     <Head>
       <title>
         {subtitle ? `${subtitle?.includes("undefined") ? title : subtitle} | Obraminha` : (title)}
       </title>
-      <meta name="description" content={description} />
+      <meta name="description" content={
+        description?.includes("undefined") ?
+          "Trabalhamos com prestadores de serviços qualificados e comprometidos. Descubra como podemos ajudá-lo a tornar seus projetos de construção realidade." : description
+      } />
       <meta property="og:title" content={subtitle ? `${subtitle === "undefined" ? title : subtitle} | Obraminha` : (title)} />
-      <meta property="og:description" content={description} />
     </Head>
   )
 }
